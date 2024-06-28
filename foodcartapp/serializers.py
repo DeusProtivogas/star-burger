59aea5fc925bf216566d7c95a7eac9eae5884fea
DeusProtivogas <kirill2689@gmail.com>
2024-06-28 11:05:05 +0300

    removed commented code

--- foodcartapp/serializers.py
+++ foodcartapp/serializers.py
@@ -11,24 +11,17 @@ from .models import OrderElement
 from .utility import fetch_coordinates
 
 
-
 class ElementsSerializer(ModelSerializer):
 
     def create(self, data):
-        print('elem data in ser ', data)
-        # print(Product.objects.get(pk=data.get('product')).price)
 
         with transaction.atomic():
             product_element = OrderElement.objects.create(
                 product=data.get('product'),
                 price=data.get('product').price,
-                # price=data.get('product').price,
                 quantity=data.get('quantity'),
-                # order=data.get('order'),
             )
 
-            print('test elem ', product_element)
-            # product_element.save()
         return product_element
 
     class Meta:
@@ -37,12 +30,9 @@ class ElementsSerializer(ModelSerializer):
 
 
 class OrderSerializer(ModelSerializer):
-    # products = ElementsSerializer(many=True, allow_empty=True, required=False)
 
     def create(self, order_details):
-        print('dets ', order_details)
         element_list = order_details.get('elements')
-        print(element_list)
 
         with transaction.atomic():
             order = Order.objects.create(
@@ -58,17 +48,11 @@ class OrderSerializer(ModelSerializer):
             order_restaurants = set(restaurants)
             element_restaurants = []
             for item in order.elements.all():
-                print(item)
-                print(item.product.menu_items.all())
                 for possible_restaurant in item.product.menu_items.all():
-                    print(possible_restaurant.restaurant.id)
                     restaurant = restaurants.filter(pk=possible_restaurant.restaurant.id)[0]
-                    print(restaurant)
                     element_restaurants.append(restaurant)
-                    print('asd', element_restaurants)
 
             order_restaurants = order_restaurants.intersection(set(element_restaurants))
-            print('chosen ', order_restaurants)
 
             if order_restaurants:
                 order.restaurants_choice = f"Доступные рестораны:\n" + '\n'.join(
@@ -77,35 +61,6 @@ class OrderSerializer(ModelSerializer):
                         for x in order_restaurants
                     ]
                 )
-
-            print("AAAA")
-            # return None
-
-            # for product in product_list:
-            #     print('b')
-            #     product_data = {
-            #         'order': order,
-            #         'product': product.get('product'),
-            #         'quantity': product.get('quantity'),
-            #     }
-            #     product_element = ElementsSerializer(data=product_data)
-            #     print(product_element)
-            #
-            #
-            #
-            # restaurants = Restaurant.objects.all()
-            # order_restaurants = set(restaurants)
-            #
-            # if not order_restaurants:
-            #     order.restaurants_choice = "Нет ресторанов"
-            # else:
-            #     order.restaurants_choice = f"Доступные рестораны:\n" + '\n'.join(
-            #         [
-            #             f'{x.name} - {round(distance.distance(fetch_coordinates(), (x.coordinates.first().latitude, x.coordinates.first().longitude)).km, 2)}'
-            #             for x in order_restaurants
-            #         ]
-            #     )
-
             order.save()
 
         return order
