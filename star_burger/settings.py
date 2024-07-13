4e0c19ec8480d4d6f971b3f0138a668bfacf151e
DeusProtivogas <kirill2689@gmail.com>
2024-07-13 12:06:57 +0300

    changed allowed hosts

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', False)
 YANDEX_API_KEY = env('YANDEX_API_KEY')
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 INSTALLED_APPS = [
     'foodcartapp.apps.FoodcartappConfig',
